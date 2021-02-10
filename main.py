1a3ac0fc14b658fd30429627a6eed8be430b0a06
gmemstr <gabriel@gitgalaxy.com>
2021-02-10 23:56:25 +0000

    Slight optimization to avoid pinning CPU.

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from PIL import Image, ImageDraw, ImageFont
 from signal import signal, SIGINT
 from sys import exit
 
+
 def handler(signal_received, frame):
     # Handle any cleanup here
     logging.info('SIGINT or CTRL-C detected. Exiting gracefully')
     epd = epd2in13_V2.EPD()
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
-    
+
     logging.info("Goto Sleep...")
     epd.sleep()
     time.sleep(3)
@@ -26,7 +27,7 @@ if __name__ == '__main__':
     signal(SIGINT, handler)
 
     logging.basicConfig(level=logging.DEBUG)
-    logging.info("epd2in13_V2 Demo")
+    logging.info("Dashboard starting")
 
     epd = epd2in13_V2.EPD()
     logging.info("init and Clear")
@@ -41,30 +42,34 @@ if __name__ == '__main__':
         time_draw = ImageDraw.Draw(time_image)
 
         epd.init(epd.FULL_UPDATE)
-        epd.displayPartBaseImage(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
-        
+        epd.displayPartBaseImage(epd.getbuffer(
+            time_image.transpose(Image.ROTATE_180)))
+
         epd.init(epd.PART_UPDATE)
         last_time = ""
         while (True):
             if time.strftime('%H:%M') == last_time:
+                time.sleep(5)
                 continue
 
             time_draw.rectangle((0, 0, 250, 90), fill=255)
             time_draw.text((0, 0), time.strftime('%H:%M'), font=font64, fill=0)
-            time_draw.text((0, 100), time.strftime('%a, %d %B (%Y)'), font=font18, fill=0)
+            time_draw.text((0, 100), time.strftime('%a, %d %B (%Y)'),
+                           font=font18, fill=0)
 
-            epd.displayPartial(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
-            epd.displayPartial(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
+            epd.displayPartial(epd.getbuffer(time_image.transpose(
+                Image.ROTATE_180)))
             last_time = time.strftime('%H:%M')
+            time.sleep(1)
         # epd.Clear(0xFF)
         logging.info("Clear...")
         epd.init(epd.FULL_UPDATE)
         epd.Clear(0xFF)
-        
+
         logging.info("Goto Sleep...")
         epd.sleep()
         time.sleep(3)
         epd.Dev_exit()
-            
+
     except IOError as e:
         logging.info(e)
