ca94e930288f6dead2b934ce83e5450b93616474
gmemstr <gabriel@gitgalaxy.com>
2021-02-10 20:54:42 +0000

    Initial commit of source.

---
+++ README.md
@@ -0,0 +1,7 @@
+E-Ink Dashboard
+===
+
+Experimental dashboard for the [Waveshare 2in13_V2](https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT)
+e-ink/e-paper display.
+
+Requires `PIL` and `spidev`
---
+++ main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import logging
+from lib import epd2in13_V2
+import time
+from PIL import Image, ImageDraw, ImageFont
+
+from signal import signal, SIGINT
+from sys import exit
+
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    logging.info('SIGINT or CTRL-C detected. Exiting gracefully')
+    epd = epd2in13_V2.EPD()
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    time.sleep(3)
+    epd.Dev_exit()
+    exit()
+
+
+if __name__ == '__main__':
+    signal(SIGINT, handler)
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("epd2in13_V2 Demo")
+
+    epd = epd2in13_V2.EPD()
+    logging.info("init and Clear")
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
+
+    font18 = ImageFont.truetype('fonts/Font.ttc', 18)
+    font64 = ImageFont.truetype('fonts/Font.ttc', 64)
+
+    try:
+        time_image = Image.new('1', (epd.height, epd.width), 255)
+        time_draw = ImageDraw.Draw(time_image)
+
+        epd.init(epd.FULL_UPDATE)
+        epd.displayPartBaseImage(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
+        
+        epd.init(epd.PART_UPDATE)
+        last_time = ""
+        while (True):
+            if time.strftime('%H:%M') == last_time:
+                continue
+
+            time_draw.rectangle((0, 0, 250, 90), fill=255)
+            time_draw.text((0, 0), time.strftime('%H:%M'), font=font64, fill=0)
+            time_draw.text((0, 100), time.strftime('%a, %d %B (%Y)'), font=font18, fill=0)
+
+            epd.displayPartial(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
+            epd.displayPartial(epd.getbuffer(time_image.transpose(Image.ROTATE_180)))
+            last_time = time.strftime('%H:%M')
+        # epd.Clear(0xFF)
+        logging.info("Clear...")
+        epd.init(epd.FULL_UPDATE)
+        epd.Clear(0xFF)
+        
+        logging.info("Goto Sleep...")
+        epd.sleep()
+        time.sleep(3)
+        epd.Dev_exit()
+            
+    except IOError as e:
+        logging.info(e)
